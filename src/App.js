8817f630aa636ee75f09701a07096e26cbdd479f
Rickard Karlsson <rickard.karlsson@appliedtechnology.se>
2018-11-01 16:13:01 +0100

    ruined the quiz functionality by adding options

--- src/App.js
+++ src/App.js
@@ -1,31 +1,87 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Menu from './Menu';
 import Quiz from './Quiz';
 
-
+// import logo from './logo.svg';
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      category: null,
+      difficulty: null,
+      qAmount: null,
+      };
+
+    this.selectCategory = this.selectCategory.bind(this);
+    this.selectDifficulty = this.selectDifficulty.bind(this);
+    this.selectQuestions = this.selectQuestions.bind(this);
+  }
+
+  selectCategory(event) {
+    this.setState({category: event.target.value});
+  }
+
+  selectDifficulty(event) {
+    this.setState({difficulty: event.target.value});
+  }
+
+  selectQuestions(event) {
+    this.setState({qAmount: event.target.value});
+  }
+
   render() {
+
+    // const generateQuiz = <button></button>
 
     return (
       <div className="App">
 
-        <header className="App-header"> 
+        <header className="App-header">
           <h1>Welcome to the Random Quiz Generator!</h1>
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          {/* <Menu /> */}
-         
+
+          <div className="menuContainer">
+            <div className="menuDiv1">
+              <label>Select category:</label><br></br>
+              <select onChange={this.selectCategory}>
+                <option value="17">Science</option>
+                <option value="18">Computers</option>
+                <option value="11">Movies</option>
+                <option value="12">Music</option>
+              </select>
+            </div>
+
+            <div className="menuDiv2">
+              <label>Select difficulty:</label><br></br>
+              <select onChange={this.selectDifficulty}>
+                <option value="easy">Easy</option>
+                <option value="medium">Medium</option>
+                <option value="hard">Hard</option>
+              </select>
+            </div>
+
+            <div className="menuDiv3">
+              <label>Select number of questions:</label><br></br>
+              <select onChange={this.selectQuestions}>
+                <option value="5">5</option>
+                <option value="10">10</option>
+                <option value="15">15</option>
+              </select>
+            </div>
+          </div>
+
+          <div id="boo"></div>
         </header>
 
         <div className="main">
-
-          <p id="boo"></p>
-
-          <Quiz />
-
+          <p></p>
+          <Quiz 
+            category={this.state.category}
+            difficulty={this.state.difficulty}
+            questionAmount={this.state.qAmount}
+          />
         </div>
 
       </div>
--- src/Quiz.js
+++ src/Quiz.js
@@ -5,18 +5,26 @@ class Quiz extends Component {
   constructor(props){
     super(props);
     this.state = {
-      spinner: 'Loading...',
       currentQuestion: 1,
       amountCorrectlyAnswered: 0,
-      category: 'Science: Computers',
-      difficulty: 'medium',
+      category: null,
+      difficulty: null,
       questionAmount: 10,
       questions: null,
     }
   }
 
   componentDidMount() {
-    fetch('https://opentdb.com/api.php?amount=10&category=18&difficulty=hard&type=multiple')
+
+    this.setState({
+      category: this.props.category,
+      difficulty: this.props.difficulty,
+      questionAmount: this.props.questionAmount,
+    });
+
+  //https://opentdb.com/api.php?amount=${this.state.questionAmount}&category=${this.state.category}&difficulty=${this.state.difficulty}&type=multiple`
+
+    fetch(`https://opentdb.com/api.php?amount=10&category=17&difficulty=hard&type=multiple`)
       .then(res => res.json())
       .then(data => {
         this.setState({ questions: data.results });
@@ -29,10 +37,16 @@ class Quiz extends Component {
         currentQuestion: this.state.currentQuestion + 1,
         amountCorrectlyAnswered: this.state.amountCorrectlyAnswered + 1,
       });
-      document.querySelector('#boo').innerHTML = 'Your answer to the previous question was correct!';
+      document
+        .querySelector('#boo')
+        .innerHTML = 
+          'Your answer to the previous question was <span id="correct">correct!</span>';
     } else {
       this.setState({currentQuestion: this.state.currentQuestion + 1})
-      document.querySelector('#boo').innerHTML = 'Your answer to the previous question was wrong.';
+      document
+        .querySelector('#boo')
+        .innerHTML = 
+          'Your answer to the previous question was <span id="wrong">wrong</span>.';
     }
   }
 
@@ -40,15 +54,24 @@ class Quiz extends Component {
     const answers = [null, null, null, null];
     const correctAnswerIndex = Math.floor(Math.random() * 4)
 
-    if ( this.state.questions !== null && this.state.currentQuestion <= this.state.questionAmount) {
+    if ( this.state.difficulty && this.state.currentQuestion <= this.state.questionAmount) {
 
-      answers[correctAnswerIndex] = this.state.questions[this.state.currentQuestion - 1].correct_answer;
+      answers[correctAnswerIndex] = 
+        this
+          .state
+          .questions[this.state.currentQuestion - 1]
+          .correct_answer;
       let wrongAnswerIndex = 0;
 
       for(let i = 0; i <= 3; i += 1) {
 
         if(answers[i] === null) { 
-          answers[i] = this.state.questions[this.state.currentQuestion - 1].incorrect_answers[wrongAnswerIndex];
+          answers[i] = 
+            this
+              .state
+              .questions[this.state.currentQuestion - 1]
+              .incorrect_answers[wrongAnswerIndex];
+
           wrongAnswerIndex += 1;
         }
 
@@ -57,51 +80,57 @@ class Quiz extends Component {
     }
 
     const quiz = (this.state.questions && this.state.currentQuestion <= this.state.questionAmount) ?
-      (<div id="quizWindow">
+      (<div className="quizWindow">
         <h3>Question {this.state.currentQuestion}:</h3>
-        <p>{ this.state.questions[this.state.currentQuestion - 1].question }</p>
-        <ol>
+        <p dangerouslySetInnerHTML={{__html: this.state.questions[this.state.currentQuestion - 1].question }}>
+        </p>
+        <ul>
           <li>
             <button 
-              onClick={() => this.playerAnswer(0, correctAnswerIndex)}>
-            { answers[0] }
+              onClick={() => this.playerAnswer(0, correctAnswerIndex)}
+              dangerouslySetInnerHTML={{ __html: `${ answers[0] }` }}>
             </button>
           </li>
           <li>
             <button 
-              onClick={() => this.playerAnswer(1, correctAnswerIndex)}>
-            { answers[1] }
+              onClick={() => this.playerAnswer(1, correctAnswerIndex)}
+              dangerouslySetInnerHTML={{ __html: `${ answers[1] }` }}
+              >
             </button>
           </li>
           <li>
             <button 
-              onClick={() => this.playerAnswer(2, correctAnswerIndex)}>
-            { answers[2] }
+              onClick={() => this.playerAnswer(2, correctAnswerIndex)}
+              dangerouslySetInnerHTML={{ __html: `${ answers[2] }` }}
+              >
             </button>
           </li>
           <li>
             <button 
-              onClick={() => this.playerAnswer(3, correctAnswerIndex)}>
-            { answers[3] }
+              onClick={() => this.playerAnswer(3, correctAnswerIndex)}
+              dangerouslySetInnerHTML={{ __html: `${ answers[3] }` }}
+              >
             </button>
           </li>
-        </ol>
+        </ul>
         
       </div>) 
       : 
       null;
 
-    if(this.state.currentQuestion > this.state.questionAmount) {
-      let finalResult = 
-      <h3>
-        You answered {this.state.amountCorrectlyAnswered}<span> </span>
-         out of {this.state.questionAmount} questions correctly
-      </h3>;
-      return finalResult;
-      
-    } else {
-      return ( quiz );
-    }
+    // if(this.state.currentQuestion > this.state.questionAmount) {
+    //   let finalResult = 
+    //     <div className="quizWindow">
+    //     <h3>
+    //       You answered {this.state.amountCorrectlyAnswered}<span> </span>
+    //       out of {this.state.questionAmount} questions correctly
+    //     </h3>
+    //     </div>;
+    //   return finalResult;
+    // } else {
+    //   console.log(this.state);
+    // }
+      return quiz;
   }
 }
 
